469466a82a699b8bc85c82cd7183caca915d5417
Duck1s <joeri.breedveld123@gmail.com>
2022-07-07 12:11:39 +0200

    feat(button): make button smaller responsive and fix responsive text on home width

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
 				<h3 className='pb-8 font-mont tracking-widest text-tertiary'>HALLO MIJN NAAM IS</h3>
 				<h1 className='text-4xl text-primary md:text-6xl pb-8 font-bold font-serif'>Joeri Breedveld</h1>
 				<h3 className='text-lg md:text-2xl pb-8 font-serif text-secondary'>Full Stack Developer | JavaScript</h3>
-				<p className='pb-24 w-1/2'>
+				<p className='pb-24 w-3/4 md:w-1/2'>
 					Hallo mijn naam is Joeri Breedveld. Ik ben een Junior Full Stack Developer en ik woon in Amsterdam. Ik programmeer sinds 2021 en ik studeer ook Software Development bij
 					Mediacollege Amsterdam. Ik vind het heel leuk om nieuwe dingen te leren en uitdagingen aan te gaan.
 				</p>
