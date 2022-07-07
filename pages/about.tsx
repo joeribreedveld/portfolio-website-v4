cbab726206e41920eaa0d27e6299d8025b2c6547
Duck1s <joeri.breedveld123@gmail.com>
2022-07-07 13:39:10 +0200

    feat(about): improve about page with colors, werkervaring, skills list style, and improve fonts

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,56 +1,108 @@
 // Imports
 import type { NextPage } from "next"
+import { FaArrowRight } from "react-icons/fa"
 
 // Functions
 const About: NextPage = () => {
 	return (
 		<div className='page-padding-x page-padding-y flex flex-col gap-16'>
 			<section>
-				<h2 className='text-2xl font-bold pb-4'>Over mij</h2>
+				<h2 className='text-2xl font-bold pb-4 text-primary font-serif'>Over mij</h2>
 				<p className='md:w-2/4'>
 					Hallo mijn naam is Joeri Breedveld. Ik ben een Junior Full Stack Developer en ik woon in Amsterdam. Ik programmeer sinds 2021 en ik studeer ook Software Development bij
 					Mediacollege Amsterdam. Ik vind het heel leuk om nieuwe dingen te leren en uitdagingen aan te gaan.
 				</p>
 			</section>
 			<section>
-				<h2 className='text-2xl font-bold pb-8'>Mijn skills</h2>
-				<ul className='flex gap-4 flex-wrap'>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>HTML</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>CSS</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>JavaScript</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>React</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>Sass</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>Laravel</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>Git</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>MERN</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>Adobe XD</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>VSCode</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>Node.js</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>Express.js</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>MongoDB</li>
-					<li className='bg-gray-100 py-3 px-6  rounded-lg'>MySQL</li>
+				<h2 className='text-2xl font-bold pb-8 font-serif text-primary'>Mijn skills</h2>
+				<ul className='flex flex-col gap-2 flex-wrap'>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> HTML
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> CSS
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> JavaScript
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> React
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> Sass
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> Laravel
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> Git
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> MERN
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> Adobe XD
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> VSCode
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> Node.js
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> Express.js
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> MongoDB
+					</li>
+					<li className='flex items-center gap-2'>
+						<FaArrowRight className='text-secondary' /> MySQL
+					</li>
 				</ul>
 			</section>
 			<section>
-				<h2 className='text-2xl font-bold pb-8'>Werkervaring</h2>
+				<h2 className='text-2xl font-bold pb-8 font-serif text-primary'>Werkervaring</h2>
 				<ul className='flex flex-col gap-8'>
 					<li className='flex flex-col gap-2'>
-						<h4 className='text-lg font-bold'>Aruna</h4>
-						<p>Beschrijving</p>
-						<ul>
-							<li>Taal</li>
-							<li>Taal</li>
+						<h4 className='text-xl font-bold text-secondary font-serif'>Aruna</h4>
+						<p>Ik ging hier samen met andere mensen uit mijn klas een Wordpress website maken.</p>
+						<ul className=''>
+							<li className='flex items-center gap-2'>
+								<FaArrowRight className='text-tertiary' /> Wordpress
+							</li>
+							<li className='flex items-center gap-2'>
+								<FaArrowRight className='text-tertiary' /> CSS
+							</li>
+							<li className='flex items-center gap-2'>
+								<FaArrowRight className='text-tertiary' /> HTML
+							</li>
 						</ul>
-						<p>Tijd</p>
+						<p className='text-quaternary font-bold'>May 2022 - June 2022</p>
+						<a href='#' className='text-quinary underline'>
+							Meer informatie
+						</a>
 					</li>
 					<li className='flex flex-col gap-2'>
-						<h4 className='text-lg font-bold'>OSD</h4>
-						<p>Beschrijving</p>
-						<ul>
-							<li>Taal</li>
-							<li>Taal</li>
+						<h4 className='text-xl font-bold text-secondary font-serif'>Oorsprongsdocumenten</h4>
+						<p>Ik werk aan een MERN ecommerce website met een support artikel systeem.</p>
+						<ul className=''>
+							<li className='flex items-center gap-2'>
+								<FaArrowRight className='text-tertiary' /> Next.js
+							</li>
+							<li className='flex items-center gap-2'>
+								<FaArrowRight className='text-tertiary' /> Node.js
+							</li>
+							<li className='flex items-center gap-2'>
+								<FaArrowRight className='text-tertiary' /> MERN
+							</li>
+							<li className='flex items-center gap-2'>
+								<FaArrowRight className='text-tertiary' /> TailwindCSS
+							</li>
 						</ul>
-						<p>Tijd</p>
+						<p className='text-quaternary font-bold'>June 2022 - Now</p>
+						<a href='#' className='text-quinary underline'>
+							Meer informatie
+						</a>
 					</li>
 				</ul>
 			</section>
