1f2baa397d1108168f211d3b8f6844402b6860ff
Duck1s <joeri.breedveld123@gmail.com>
2022-07-07 17:09:26 +0200

    feat(projects): add projects api fetch map, add skill list map fetch, json fetching and projects.json with some example projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,52 +1,37 @@
 // Imports
 import type { NextPage } from "next"
 import { FaArrowRight } from "react-icons/fa"
+import projects from "../public/assets/json/projects.json"
 
 // Functions
 const Projects: NextPage = () => {
+	const projectsList = projects.map((project) => {
+		const skillList = project[0].skills.map((skill) => {
+			return (
+				<li className='flex items-center gap-2'>
+					<FaArrowRight className='text-tertiary' /> {skill}
+				</li>
+			)
+		})
+
+		return (
+			<li className='flex flex-col gap-2'>
+				<h4 className='text-xl font-bold text-secondary'>{project[0].title}</h4>
+				<p>{project[0].intro}</p>
+
+				<ul className=''>{skillList}</ul>
+				<p className='text-quaternary font-bold'>{project[0].date}</p>
+				<a href='#' className='text-quinary underline'>
+					Meer informatie
+				</a>
+			</li>
+		)
+	})
+
 	return (
 		<div className='page-padding-x page-padding-y'>
 			<h2 className='text-2xl font-bold pb-8 text-primary'>Projecten</h2>
-			<ul className='flex flex-col gap-8'>
-				<li className='flex flex-col gap-2'>
-					<h4 className='text-xl font-bold text-secondary'>oorsprongsdocumenten.nl</h4>
-					<p>Ik werk aan een MERN ecommerce website met een support artikel systeem.</p>
-					<ul className=''>
-						<li className='flex items-center gap-2'>
-							<FaArrowRight className='text-tertiary' /> Next.js
-						</li>
-						<li className='flex items-center gap-2'>
-							<FaArrowRight className='text-tertiary' /> Node.js
-						</li>
-						<li className='flex items-center gap-2'>
-							<FaArrowRight className='text-tertiary' /> MERN
-						</li>
-						<li className='flex items-center gap-2'>
-							<FaArrowRight className='text-tertiary' /> TailwindCSS
-						</li>
-					</ul>
-					<p className='text-quaternary font-bold'>June 2022 - Now</p>
-					<a href='#' className='text-quinary underline'>
-						Meer informatie
-					</a>
-				</li>
-				<li className='flex flex-col gap-2'>
-					<h4 className='text-xl font-bold text-secondary'>joeribreedveld.com</h4>
-					<p>Ik werk aan een MERN ecommerce website met een support artikel systeem.</p>
-					<ul className=''>
-						<li className='flex items-center gap-2'>
-							<FaArrowRight className='text-tertiary' /> Next.js
-						</li>
-						<li className='flex items-center gap-2'>
-							<FaArrowRight className='text-tertiary' /> TailwindCSS
-						</li>
-					</ul>
-					<p className='text-quaternary font-bold'>Juli 2022 - Now</p>
-					<a href='#' className='text-quinary underline'>
-						Meer informatie
-					</a>
-				</li>
-			</ul>
+			<ul className='flex flex-col gap-8'>{projectsList}</ul>
 		</div>
 	)
 }
