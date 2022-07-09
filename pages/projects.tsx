71d3e446ba6db4e2f933f611b7ec4173b6779739
Duck1s <joeri.breedveld123@gmail.com>
2022-07-09 23:10:41 +0200

    feat(projects): change empty links in json, add conditional link rendering and style accordingly

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -21,15 +21,23 @@ const Projects: NextPage = () => {
 				<td className='py-4 px-6 font-bold'>{project[0].title}</td>
 				<td className='py-4 px-6 text-gray-600'>{project[0].madeat}</td>
 				<td className='py-4 px-6 text-gray-600'>{skillList}</td>
-				<td className='py-4 px-6'>
-					<a href={project[0].live} target='_blank' rel='noreferrer'>
-						<FiExternalLink size={24} className='text-gray-600' />
-					</a>
+				<td className='py-4 px-6 pr-0'>
+					{project[0].live ? (
+						<a href={project[0].live} target='_blank' rel='noreferrer'>
+							<FiExternalLink size={24} className='text-gray-600' />
+						</a>
+					) : (
+						""
+					)}
 				</td>
-				<td className='py-4 px-6 rounded-r-lg'>
-					<a href={project[0].github} target='_blank' rel='noreferrer'>
-						<FiGithub size={24} className='text-gray-600' />
-					</a>
+				<td className='py-4 pl-0 px-6 rounded-r-lg'>
+					{project[0].github ? (
+						<a href={project[0].github} target='_blank' rel='noreferrer'>
+							<FiGithub size={24} className='text-gray-600' />
+						</a>
+					) : (
+						""
+					)}
 				</td>
 			</tr>
 		)
@@ -46,7 +54,7 @@ const Projects: NextPage = () => {
 						<th className='py-4 px-6'>Titel</th>
 						<th className='py-4 px-6'>Gemaakt bij</th>
 						<th className='py-4 px-6'>Gemaakt met</th>
-						<th className='py-4 px-6 text-center'>Link</th>
+						<th className='py-4 px-6'>Link</th>
 					</tr>
 				</thead>
 				<tbody>{projectsList}</tbody>
