bdbf23cb52ae205dc864875bbda31533ef100a92
Duck1s <joeri.breedveld123@gmail.com>
2022-07-09 23:40:26 +0200

    feat(projects): trying to get the table responsive, change padding, change table css

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -9,7 +9,7 @@ const Projects: NextPage = () => {
 	const projectsList = projects.map((project) => {
 		const skillList = project[0].skills.map((skill) => {
 			return (
-				<span key={skill} className='mr-6 text-sm'>
+				<span key={skill} className='text-sm pr-4'>
 					{skill}
 				</span>
 			)
@@ -17,11 +17,11 @@ const Projects: NextPage = () => {
 
 		return (
 			<tr key={project[0].id} className='hover:bg-gray-100'>
-				<td className='py-4 px-4 rounded-l-lg text-secondary font-mono'>{project[0].date}</td>
-				<td className='py-4 px-6 font-bold'>{project[0].title}</td>
-				<td className='py-4 px-6 text-gray-600'>{project[0].madeat}</td>
-				<td className='py-4 px-6 text-gray-600'>{skillList}</td>
-				<td className='py-4 px-6 pr-0'>
+				<td className='py-4 px-2 rounded-l-lg text-secondary font-mono'>{project[0].date}</td>
+				<td className='py-4 px-2 font-bold'>{project[0].title}</td>
+				<td className='py-4 px-2 text-gray-600 hidden md:table-cell'>{project[0].madeat}</td>
+				<td className='py-4 px-2 text-gray-600 hidden md:table-cell'>{skillList}</td>
+				<td className='py-4 px-2 pr-0'>
 					{project[0].live ? (
 						<a href={project[0].live} target='_blank' rel='noreferrer'>
 							<FiExternalLink size={24} className='text-gray-600' />
@@ -30,7 +30,7 @@ const Projects: NextPage = () => {
 						""
 					)}
 				</td>
-				<td className='py-4 pl-0 px-6 rounded-r-lg'>
+				<td className='py-4 pl-0 px-2 rounded-r-lg'>
 					{project[0].github ? (
 						<a href={project[0].github} target='_blank' rel='noreferrer'>
 							<FiGithub size={24} className='text-gray-600' />
@@ -47,18 +47,20 @@ const Projects: NextPage = () => {
 		<div className='page-padding-x page-padding-y'>
 			<h2 className='text-2xl font-bold pb-4 text-primary'>Projecten</h2>
 			<p className='pb-16 text-secondary text-lg'>Dit is een archief van al mijn projecten.</p>
-			<table className='table-auto text-left w-full'>
-				<thead className='text-gray-600 font-bold'>
-					<tr>
-						<th className='py-4 px-4'>Datum</th>
-						<th className='py-4 px-6'>Titel</th>
-						<th className='py-4 px-6'>Gemaakt bij</th>
-						<th className='py-4 px-6'>Gemaakt met</th>
-						<th className='py-4 px-6'>Link</th>
-					</tr>
-				</thead>
-				<tbody>{projectsList}</tbody>
-			</table>
+			<div className='overflow-auto'>
+				<table className='table text-left w-full border-collapse overflow-auto'>
+					<thead className='text-gray-600 font-bold'>
+						<tr>
+							<th className='py-4 px-2'>Datum</th>
+							<th className='py-4 px-2'>Titel</th>
+							<th className='py-4 px-2 hidden md:table-cell'>Gemaakt bij</th>
+							<th className='py-4 px-2 hidden md:table-cell'>Gemaakt met</th>
+							<th className='py-4 px-2'>Link</th>
+						</tr>
+					</thead>
+					<tbody>{projectsList}</tbody>
+				</table>
+			</div>
 		</div>
 	)
 }
