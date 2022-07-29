9ea4d8599083f3051869e15a70f3feb3431fc989
Duck1s <joeri.breedveld123@gmail.com>
2022-07-29 12:00:15 +0200

    fix(projects): change responsiveness with hidden cells and text size

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -12,7 +12,7 @@ const Projects: NextPage = () => {
 	const projectsList = projects.map((project) => {
 		const skillList = project.skills.map((skill) => {
 			return (
-				<span key={skill} className='text-sm pr-4'>
+				<span key={skill} className='text-sm pr-1'>
 					{skill}
 				</span>
 			)
@@ -29,7 +29,7 @@ const Projects: NextPage = () => {
 				<td className='py-4 px-2 rounded-l-lg text-secondary font-mono'>{project.date}</td>
 				<td className='py-4 px-2 font-bold'>{project.title}</td>
 				<td className='py-4 px-2 text-gray-600 hidden md:table-cell'>{project.madeat}</td>
-				<td className='py-4 px-2 text-gray-600 hidden md:table-cell'>{skillList}</td>
+				<td className='py-4 px-2 text-gray-600 hidden xl:table-cell'>{skillList}</td>
 				<td className='py-4 px-2 pr-0'>
 					{project.live ? (
 						<a href={project.live} target='_blank' rel='noreferrer'>
@@ -57,13 +57,13 @@ const Projects: NextPage = () => {
 			<h2 className='text-2xl font-bold pb-4 text-primary'>Projecten</h2>
 			<p className='pb-16 text-secondary text-lg'>Dit is een archief van al mijn projecten.</p>
 			<div className='overflow-auto'>
-				<table className='table text-left w-full border-collapse overflow-auto'>
+				<table className='table text-left w-full border-collapse overflow-auto text-sm md:text-base'>
 					<thead className='text-gray-600 font-bold'>
 						<tr>
 							<th className='py-4 px-2'>Datum</th>
 							<th className='py-4 px-2'>Titel</th>
 							<th className='py-4 px-2 hidden md:table-cell'>Gemaakt bij</th>
-							<th className='py-4 px-2 hidden md:table-cell'>Gemaakt met</th>
+							<th className='py-4 px-2 hidden xl:table-cell'>Gemaakt met</th>
 							<th className='py-4 px-2'>Link</th>
 						</tr>
 					</thead>
