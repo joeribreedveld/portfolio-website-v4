d28c525434ed6eab422c52ca7314ab39259e87f2
Duck1s <joeri.breedveld123@gmail.com>
2022-07-29 12:04:49 +0200

    feat(meta): add a lot of meta data to the head for seo optimization

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,9 +13,23 @@ function Layout({ children }: LayoutProps) {
 	return (
 		<>
 			<Head>
-				<meta charSet='utf-8' />
-				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+				<meta charSet='UTF-8' key='charset' />
+				<meta name='viewport' content='width=device-width,initial-scale=1' key='viewport' />
+				<meta name='description' content='Joeri Breedveld Full Stack Developer Portfolio Website' />
+				<link rel='icon' href='/favicon.ico' />
 				<title>Joeri Breedveld | Full Stack Developer</title>
+
+				<meta property='og:url' content='https://joeribreedveld.com/' />
+				<meta property='og:type' content='website' />
+				<meta property='og:title' content='Joeri Breedveld | Full Stack Developer' />
+				<meta property='og:description' content="Hello, I'm a full stack JavaScript web developer based in Amsterdam. Take a look at my experience and projects!" />
+				<meta property='og:image' content='https://cdn.ttgtmedia.com/rms/onlineimages/code_g1019737194.jpg' />
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta property='twitter:domain' content='joeribreedveld.com' />
+				<meta property='twitter:url' content='https://joeribreedveld.com/' />
+				<meta name='twitter:title' content='Joeri Breedveld | Full Stack Developer' />
+				<meta name='twitter:description' content="Hello, I'm a full stack JavaScript web developer based in Amsterdam. Take a look at my experience and projects!" />
+				<meta name='twitter:image' content='https://cdn.ttgtmedia.com/rms/onlineimages/code_g1019737194.jpg' />
 			</Head>
 			<header>
 				<Navbar />
