303f62f287d38667730a964ecf52c03210c95b5d
Duck1s <32653@ma-web.nl>
2022-08-06 11:47:16 +0200

    feat(skills): start on skills, change gray background tint

--- pages/about.tsx
+++ pages/about.tsx
@@ -20,15 +20,15 @@ const About: NextPage = () => {
 			<section className=''>
 				<h2 className='text-4xl font-bold pb-8 text-primary'>Mijn skills</h2>
 				<ul className='grid md:grid-cols-3 md:grid-rows-3 grid-cols-1 grid-rows-9 gap-6'>
-					<div className='bg-gray p-8 rounded-xl'>Test</div>
-					<div className='bg-gray p-8 rounded-xl'>Test</div>
-					<div className='bg-gray p-8 rounded-xl'>Test</div>
-					<div className='bg-gray p-8 rounded-xl'>Test</div>
-					<div className='bg-gray p-8 rounded-xl'>Test</div>
-					<div className='bg-gray p-8 rounded-xl'>Test</div>
-					<div className='bg-gray p-8 rounded-xl'>Test</div>
-					<div className='bg-gray p-8 rounded-xl'>Test</div>
-					<div className='bg-gray p-8 rounded-xl'>Test</div>
+					<div className='bg-[#f8f8f8] p-8 rounded-xl'>Test</div>
+					<div className='bg-[#f8f8f8] p-8 rounded-xl'>Test</div>
+					<div className='bg-[#f8f8f8] p-8 rounded-xl'>Test</div>
+					<div className='bg-[#f8f8f8] p-8 rounded-xl'>Test</div>
+					<div className='bg-[#f8f8f8] p-8 rounded-xl'>Test</div>
+					<div className='bg-[#f8f8f8] p-8 rounded-xl'>Test</div>
+					<div className='bg-[#f8f8f8] p-8 rounded-xl'>Test</div>
+					<div className='bg-[#f8f8f8] p-8 rounded-xl'>Test</div>
+					<div className='bg-[#f8f8f8] p-8 rounded-xl'>Test</div>
 				</ul>
 			</section>
 		</div>
