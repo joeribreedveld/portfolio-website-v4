7142fcee597949711ee0f4c41972bc912b568a5e
Duck1s <32653@ma-web.nl>
2022-09-05 13:35:00 +0200

    fix(hero): fix social link gap width

--- categories/Hero.tsx
+++ categories/Hero.tsx
@@ -10,7 +10,7 @@ function Hero() {
 			<section className='page-padding-x page-padding-y bg-black text-white min-h-screen flex justify-center flex-col'>
 				<h1 className='text-primary font-bold text-5xl mb-4'>Joeri Breedveld</h1>
 				<h3 className='text-xl mb-16'>Full Stack Developer</h3>
-				<ul className='flex navbar-gap'>
+				<ul className='flex gap-16'>
 					<li>
 						<FiGithub size={24} />
 					</li>
