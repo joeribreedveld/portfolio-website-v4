2d45a70a8a68fcb723cac526a5d1236cae40d8ed
Duck1s <32653@ma-web.nl>
2022-09-06 11:50:59 +0200

    fix(hero): change job title location

--- categories/Hero.tsx
+++ categories/Hero.tsx
@@ -8,7 +8,7 @@ function Hero() {
 	return (
 		<>
 			<section className='page-padding-x page-padding-y bg-black text-white min-h-screen flex justify-center flex-col'>
-				<h1 className='text-primary font-bold text-4xl md:text-5xl mb-4'>Joeri Breedveld</h1>
+				<h1 className='text-primary font-bold text-4xl md:text-5xl mb-8'>Joeri Breedveld</h1>
 				<h3 className='text-xl mb-16'>Full Stack Developer</h3>
 				<ul className='flex gap-12'>
 					<li>
