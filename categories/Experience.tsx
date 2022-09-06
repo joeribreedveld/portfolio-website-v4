4aefca8ec5c4db3fea05478b93cb39eb64bd6793
Duck1s <32653@ma-web.nl>
2022-09-06 11:48:40 +0200

    fix(padding): change padding for mobile responsive screens

--- categories/Experience.tsx
+++ categories/Experience.tsx
@@ -22,7 +22,7 @@ function Experience() {
 		<>
 			<section className='page-padding-x page-padding-y bg-gray'>
 				<h2 className='font-bold text-2xl mb-8'>Mijn werkervaring</h2>
-				<ul className='grid grid-cols-1 md:grid-cols-2 gap-8'>{experienceList}</ul>
+				<ul className='grid grid-cols-1 md:grid-cols-2 gap-4'>{experienceList}</ul>
 			</section>
 		</>
 	)
--- categories/Skills.tsx
+++ categories/Skills.tsx
@@ -17,7 +17,7 @@ function Skills() {
 		<>
 			<section className='page-padding-x page-padding-y bg-gray'>
 				<h2 className='font-bold text-2xl mb-8'>Mijn skills</h2>
-				<ul className='grid grid-cols-2 md:grid-cols-3 gap-8 mb-8'>{skillList}</ul>
+				<ul className='grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-8 mb-8'>{skillList}</ul>
 				<p className='text-primary underline'>Check mijn GitHub</p>
 			</section>
 		</>
