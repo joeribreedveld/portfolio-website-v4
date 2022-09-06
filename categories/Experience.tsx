cb3a8b7484f5a0709b08b6fe5c642dfb62a91ddd
Duck1s <32653@ma-web.nl>
2022-09-06 11:12:54 +0200

    feat(img): change image location structure

--- categories/Experience.tsx
+++ categories/Experience.tsx
@@ -5,8 +5,8 @@ function Experience() {
 	return (
 		<>
 			<section className='page-padding-x page-padding-y bg-gray'>
-				<h2 className='font-bold text-2xl mb-4'>Mijn werkervaring</h2>
-				<p>Hier komt mijn werkervaring</p>
+				<h2 className='font-bold text-2xl mb-8'>Mijn werkervaring</h2>
+				<ul className='grid grid-cols-1 md:grid-cols-2'></ul>
 			</section>
 		</>
 	)
