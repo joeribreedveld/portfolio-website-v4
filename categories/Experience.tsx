d4fc124bd8d5079ba06ef209839cc52bb899bec8
Duck1s <32653@ma-web.nl>
2022-09-06 11:32:41 +0200

    feat(projects): add projects images and change rounded and add more gap and change logo location and change experiences json names

--- categories/Experience.tsx
+++ categories/Experience.tsx
@@ -1,12 +1,28 @@
 // Imports
+import experiences from "../public/json/experiences.json"
 
 // Functions
 function Experience() {
+	const experienceList = experiences.map((experience) => {
+		return (
+			<>
+				<li className='bg-white p-8 rounded-xl' key={experience.id}>
+					<h3 className='text-xl font-bold mb-4'>{experience.name}</h3>
+					<p className='mb-4 text-secondary font-bold'>{experience.time}</p>
+					<p className='mb-4'>{experience.intro}</p>
+					<a href='#' className='text-primary underline hover:text-secondary'>
+						Meer informatie
+					</a>
+				</li>
+			</>
+		)
+	})
+
 	return (
 		<>
 			<section className='page-padding-x page-padding-y bg-gray'>
 				<h2 className='font-bold text-2xl mb-8'>Mijn werkervaring</h2>
-				<ul className='grid grid-cols-1 md:grid-cols-2'></ul>
+				<ul className='grid grid-cols-1 md:grid-cols-2 gap-8'>{experienceList}</ul>
 			</section>
 		</>
 	)
