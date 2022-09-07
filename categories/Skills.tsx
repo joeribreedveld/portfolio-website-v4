f94d29cf5d192cf0127c9231fd3beac60a2ec544
Duck1s <32653@ma-web.nl>
2022-09-07 15:32:13 +0200

    feat(skills): add skills images and make it look good

--- categories/Skills.tsx
+++ categories/Skills.tsx
@@ -6,8 +6,9 @@ function Skills() {
 	const skillList = skills.map((skill) => {
 		return (
 			<>
-				<li key={skill.id} className='bg-white rounded p-8'>
-					{skill.name}
+				<li key={skill.id} className='bg-white rounded p-8 items-center flex flex-col justify-center text-center'>
+					<img src={skill.img} alt='Skill image icon' className='h-12 mb-4' />
+					<p>{skill.name}</p>
 				</li>
 			</>
 		)
@@ -17,7 +18,7 @@ function Skills() {
 		<>
 			<section className='page-padding-x page-padding-y bg-gray' id='skills'>
 				<h2 className='font-bold text-2xl mb-8'>Mijn skills</h2>
-				<ul className='grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-8 mb-8'>{skillList}</ul>
+				<ul className='grid grid-cols-2 md:grid-cols-4 gap-6 mb-8'>{skillList}</ul>
 				<a href='https://github.com/joeribreedveld' target='_blank' className='text-primary underline hover:text-secondary'>
 					Check mijn GitHub
 				</a>
