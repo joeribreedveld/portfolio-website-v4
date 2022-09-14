17e5f81415bf430cc8dc3ef91bb6c0408b6d860c
Duck1s <32653@ma-web.nl>
2022-09-14 09:33:32 +0200

    feat(routing): add basic routing with contact placeholder

---
+++ pages/projects/[project].tsx
@@ -0,0 +1,33 @@
+// Imports
+
+// Functions
+function Contact() {
+	return (
+		<>
+			<section className='page-padding-x page-padding-y bg-white' id='contact'>
+				<h2 className='font-bold text-2xl mb-4'>Contact</h2>
+				<p className='mb-6'>Contact mij voor meer informatie of vragen.</p>
+				<ul>
+					<li className='mb-4'>
+						<a className='text-primary underline hover:text-secondary' href='https://github.com/joeribreedveld' target='_blank'>
+							GitHub
+						</a>
+					</li>
+					<li className='mb-4'>
+						<a className='text-primary underline hover:text-secondary' href='mailto:joeri.breedveld123@gmail.com' target='_blank'>
+							Email
+						</a>
+					</li>
+					<li className='mb-4'>
+						<a className='text-primary underline hover:text-secondary' href='https://www.linkedin.com/in/joeribreedveld/' target='_blank'>
+							LinkedIn
+						</a>
+					</li>
+				</ul>
+			</section>
+		</>
+	)
+}
+
+// Exports
+export default Contact
