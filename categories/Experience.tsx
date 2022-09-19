49e7ad8389d1232dfd8b97a1d5037c59a6888257
Duck1s <32653@ma-web.nl>
2022-09-19 22:19:36 +0200

    feat(experience): add dynamic routes for experiences with basic content

--- categories/Experience.tsx
+++ categories/Experience.tsx
@@ -12,7 +12,7 @@ function Experience() {
 						<h3 className='text-lg md:text-xl font-bold mb-4'>{experience.name}</h3>
 						<p className='mb-4 text-secondary font-bold'>{experience.time}</p>
 						<p className='mb-4'>{experience.intro}</p>
-						<a href='#' className='text-primary underline hover:text-secondary'>
+						<a href={"/experiences/" + experience.slug} className='text-primary underline hover:text-secondary'>
 							Meer informatie
 						</a>
 					</div>
