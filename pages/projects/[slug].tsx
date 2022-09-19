a086353bf9709d6ff3a3111cfaeddf6dfdb88acc
Duck1s <32653@ma-web.nl>
2022-09-19 21:46:31 +0200

    feat(navbar): make redirect to home if not on home for dynamic routes

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -9,7 +9,7 @@ const Project: NextPage = ({ project }: any) => {
 	return (
 		<>
 			<div className='bg-white min-h-[100vh] page-padding-x page-padding-y'>
-				<h3 className='text-xl font-bold mb-4'>{project.title}</h3>
+				<h3 className='text-2xl font-bold mb-4'>{project.title}</h3>
 				<p>{project.description}</p>
 			</div>
 		</>
