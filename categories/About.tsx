19186c1a07bf2a8997d917196c488a9103fd4c82
Duck1s <32653@ma-web.nl>
2022-10-05 10:45:33 +0200

    feat(about): add what i want to do to about page

--- categories/About.tsx
+++ categories/About.tsx
@@ -7,9 +7,13 @@ function About() {
 		<>
 			<section className='page-padding-x page-padding-y bg-white' id='about'>
 				<h2 className='font-bold text-2xl mb-6'>Over mij</h2>
-				<p className='mb-4'>
+				<p className='mb-8'>
 					Hallo mijn naam is Joeri Breedveld. Ik ben een Junior Full Stack Developer en ik woon in Amsterdam. Ik programmeer sinds 2021 en ik studeer Software Development aan Mediacollege
 					Amsterdam. Ik vind het heel leuk om nieuwe dingen te leren. In mijn vrije tijd werk ik veel aan Next.js en andere skills om beter te worden in het maken van leuke projecten.
+				</p>
+				<p className='mb-4'>
+					Ik ben erg leergierig en wil graag meer leren over backend development. Ik probeer dagelijks te werken aan frontend en backend roadmaps. Voordat ik veel met backend ga doen wil ik
+					eerst goed kunnen werken met een paar frontend frameworks zodat ik goed weet hoe de backend kan worden gebruikt in de frontend.
 				</p>
 				<Link to='projects' spy={true} smooth={true} offset={-70} duration={500}>
 					<p className='text-primary underline hover:cursor-pointer hover:text-secondary'>Bekijk mijn projecten</p>
