f3a3d4779ca1cabe521adf990145520a10c1500e
Duck1s <32653@ma-web.nl>
2022-10-25 10:59:01 +0200

    feat(experience): add link to website to json data and project page

--- pages/experiences/[slug].tsx
+++ pages/experiences/[slug].tsx
@@ -1,49 +1,64 @@
 // Imports
-import type { GetStaticProps, NextPage } from "next"
-import experiences from "../../public/json/experiences.json"
-import { useRouter } from "next/router"
-import Link from "next/link"
+import type { GetStaticProps, NextPage } from "next";
+import experiences from "../../public/json/experiences.json";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 // Functions
 const Experience: NextPage = ({ experience }: any) => {
-	console.log(experience)
-	return (
-		<>
-			<div className='bg-white min-h-[100vh] page-padding-x page-padding-y'>
-				<Link href='/#experience'>
-					<p className='block mb-8 underline text-primary hover:text-secondary hover:cursor-pointer'>Ga terug</p>
-				</Link>
-				<img src={experience.img} alt='experience image' className='object-cover h-96 w-full rounded-lg mb-8 shadow-lg' />
-				<h3 className='text-2xl font-bold mb-4'>{experience.title}</h3>
-				<p className='mb-8 font-bold text-slate-500'>{experience.time}</p>
-				<p>{experience.description}</p>
-			</div>
-		</>
-	)
-}
+  console.log(experience);
+  return (
+    <>
+      <div className="bg-white min-h-[100vh] page-padding-x page-padding-y">
+        <Link href="/#experience">
+          <p className="block mb-8 underline text-primary hover:text-secondary hover:cursor-pointer">
+            Ga terug
+          </p>
+        </Link>
+        <img
+          src={experience.img}
+          alt="experience image"
+          className="object-cover h-96 w-full rounded-lg mb-8 shadow-lg"
+        />
+        <h3 className="text-2xl font-bold mb-4">{experience.title}</h3>
+        <p className="mb-4 font-bold text-slate-500">{experience.time}</p>
+        <a
+          className="mb-8 text-primary underline hover:text-secondary"
+          href={experience.link}
+          target="_blank"
+        >
+          Link naar website
+        </a>
+        <p className="mt-6">{experience.description}</p>
+      </div>
+    </>
+  );
+};
 
 // getStaticPaths
 export async function getStaticPaths() {
-	const paths = experiences.map((experience) => {
-		return { params: { slug: experience.slug } }
-	})
+  const paths = experiences.map((experience) => {
+    return { params: { slug: experience.slug } };
+  });
 
-	return {
-		paths,
-		fallback: false,
-	}
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 // getStaticProps
 export const getStaticProps: GetStaticProps = async ({ params }: any) => {
-	const experience = experiences.filter((experience) => experience.slug === params.slug)
+  const experience = experiences.filter(
+    (experience) => experience.slug === params.slug
+  );
 
-	return {
-		props: {
-			experience: experience[0],
-		},
-	}
-}
+  return {
+    props: {
+      experience: experience[0],
+    },
+  };
+};
 
 // Exports
-export default Experience
+export default Experience;
--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,70 +1,92 @@
 // Imports
-import type { GetStaticProps, NextPage } from "next"
-import projects from "../../public/json/projects.json"
-import Link from "next/link"
-import { FiGithub } from "react-icons/fi"
-import { FiExternalLink } from "react-icons/fi"
+import type { GetStaticProps, NextPage } from "next";
+import projects from "../../public/json/projects.json";
+import Link from "next/link";
+import { FiGithub } from "react-icons/fi";
+import { FiExternalLink } from "react-icons/fi";
 
 // Functions
 const Project: NextPage = ({ project }: any) => {
-	return (
-		<>
-			<div className='bg-white min-h-[100vh] page-padding-x page-padding-y'>
-				<Link href='/#projects'>
-					<p className='block mb-8 underline text-primary hover:text-secondary hover:cursor-pointer'>Ga terug</p>
-				</Link>
-				<img src={project.img} alt='Project image' className='object-cover h-96 w-full rounded-lg mb-8 shadow-lg' />
-				<h3 className='text-2xl font-bold mb-4'>{project.title}</h3>
-				<p className='mb-6 font-bold text-slate-500'>{project.date}</p>
-				<ul className='mb-8 flex gap-6 items-center'>
-					{project.github ? (
-						<li>
-							<a className='' href={project.github} target='_blank' rel='noreferrer'>
-								<FiGithub size={24} className='hover:text-primary transition ease-in-out duration-200' />
-							</a>
-						</li>
-					) : (
-						""
-					)}
-					{project.live ? (
-						<li>
-							<a className='' href={project.live} target='_blank' rel='noreferrer'>
-								<FiExternalLink size={24} className='hover:text-primary transition ease-in-out duration-200' />
-							</a>
-						</li>
-					) : (
-						""
-					)}
-				</ul>
+  return (
+    <>
+      <div className="bg-white min-h-[100vh] page-padding-x page-padding-y">
+        <Link href="/#projects">
+          <p className="block mb-8 underline text-primary hover:text-secondary hover:cursor-pointer">
+            Ga terug
+          </p>
+        </Link>
+        <img
+          src={project.img}
+          alt="Project image"
+          className="object-cover h-96 w-full rounded-lg mb-8 shadow-lg"
+        />
+        <h3 className="text-2xl font-bold mb-4">{project.title}</h3>
+        <p className="mb-6 font-bold text-slate-500">{project.date}</p>
+        <ul className="mb-8 flex gap-6 items-center">
+          {project.github ? (
+            <li>
+              <a
+                className=""
+                href={project.github}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FiGithub
+                  size={24}
+                  className="hover:text-primary transition ease-in-out duration-200"
+                />
+              </a>
+            </li>
+          ) : (
+            ""
+          )}
+          {project.live ? (
+            <li>
+              <a
+                className=""
+                href={project.live}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FiExternalLink
+                  size={24}
+                  className="hover:text-primary transition ease-in-out duration-200"
+                />
+              </a>
+            </li>
+          ) : (
+            ""
+          )}
+        </ul>
 
-				<p>{project.description}</p>
-			</div>
-		</>
-	)
-}
+        <p>{project.description}</p>
+      </div>
+    </>
+  );
+};
 
 // getStaticPaths
 export async function getStaticPaths() {
-	const paths = projects.map((project) => {
-		return { params: { slug: project.slug } }
-	})
+  const paths = projects.map((project) => {
+    return { params: { slug: project.slug } };
+  });
 
-	return {
-		paths,
-		fallback: false,
-	}
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 // getStaticProps
 export const getStaticProps: GetStaticProps = async ({ params }: any) => {
-	const project = projects.filter((project) => project.slug === params.slug)
+  const project = projects.filter((project) => project.slug === params.slug);
 
-	return {
-		props: {
-			project: project[0],
-		},
-	}
-}
+  return {
+    props: {
+      project: project[0],
+    },
+  };
+};
 
 // Exports
-export default Project
+export default Project;
