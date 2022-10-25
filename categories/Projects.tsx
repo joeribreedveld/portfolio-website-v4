8f0669aefc8be32bbbf2ca159da7c61fdbe950e1
Duck1s <32653@ma-web.nl>
2022-10-25 22:08:26 +0200

    feat(projects): make array sort for dates and change date format

--- categories/Projects.tsx
+++ categories/Projects.tsx
@@ -3,7 +3,25 @@ import projects from "../public/json/projects.json";
 
 // Functions
 function Projects() {
-  const projectList = projects.map((project) => {
+  const customSort = (a: any, b: any) => {
+    a = a.date.toString();
+    b = b.date.toString();
+    a = a.split("-");
+    b = b.split("-");
+    const dateA = new Date(a[1], a[0], 1);
+    const dateB = new Date(b[1], b[0], 1);
+    if (dateA > dateB) {
+      return -1;
+    }
+    if (dateA < dateB) {
+      return 1;
+    }
+    return 0;
+  };
+
+  const projectsData = projects.sort(customSort);
+
+  const projectList = projectsData.map((project) => {
     return (
       <>
         <li key={project.id}>
@@ -32,8 +50,8 @@ function Projects() {
         <h2 className="font-bold text-2xl mb-4">Mijn projecten</h2>
         <p className="pb-10 md:w-1/2">
           Dit is een lijst van mijn projecten. Je kan per project een
-          uitgebreide pagina bekijken met meer informatie. Onderaan de pagina
-          staan de meest recente projecten.
+          uitgebreide pagina bekijken met meer informatie. De projecten zijn
+          gesorteerd op datum van nieuwe naar oud.
         </p>
         <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-16">
           {projectList}
