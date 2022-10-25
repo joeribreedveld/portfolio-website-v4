3ba216e3e8798e57622704a2ad41200a30a48f85
Duck1s <32653@ma-web.nl>
2022-10-25 10:25:21 +0200

    change job title

--- categories/Hero.tsx
+++ categories/Hero.tsx
@@ -1,36 +1,53 @@
 // Imports
-import { FiGithub } from "react-icons/fi"
-import { FiMail } from "react-icons/fi"
-import { AiFillLinkedin } from "react-icons/ai"
+import { FiGithub } from "react-icons/fi";
+import { FiMail } from "react-icons/fi";
+import { AiFillLinkedin } from "react-icons/ai";
 
 // Functions
 function Hero() {
-	return (
-		<>
-			<section className='page-padding-x page-padding-y bg-black text-white min-h-screen flex justify-center items-center flex-col text-center md:text-left' id='hero'>
-				<h1 className='text-primary font-bold text-4xl md:text-6xl mb-8'>Joeri Breedveld</h1>
-				<h3 className='text-xl mb-16'>Front-End Developer</h3>
-				<ul className='flex gap-12 justify-center md:justify-start'>
-					<li>
-						<a href='https://github.com/joeribreedveld' target='_blank'>
-							<FiGithub size={24} className='hover:text-primary transition ease-in-out duration-200' />
-						</a>
-					</li>
-					<li>
-						<a href='mailto:joeri.breedveld123@gmail.com' target='_blank'>
-							<FiMail size={24} className='hover:text-primary transition ease-in-out duration-200' />
-						</a>
-					</li>
-					<li>
-						<a href='https://www.linkedin.com/in/joeribreedveld/' target='_blank'>
-							<AiFillLinkedin size={24} className='hover:text-primary transition ease-in-out duration-200' />
-						</a>
-					</li>
-				</ul>
-			</section>
-		</>
-	)
+  return (
+    <>
+      <section
+        className="page-padding-x page-padding-y bg-black text-white min-h-screen flex justify-center items-center flex-col text-center md:text-left"
+        id="hero"
+      >
+        <h1 className="text-primary font-bold text-4xl md:text-6xl mb-8">
+          Joeri Breedveld
+        </h1>
+        <h3 className="text-xl mb-16">Full Stack Developer</h3>
+        <ul className="flex gap-12 justify-center md:justify-start">
+          <li>
+            <a href="https://github.com/joeribreedveld" target="_blank">
+              <FiGithub
+                size={24}
+                className="hover:text-primary transition ease-in-out duration-200"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:joeri.breedveld123@gmail.com" target="_blank">
+              <FiMail
+                size={24}
+                className="hover:text-primary transition ease-in-out duration-200"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/joeribreedveld/"
+              target="_blank"
+            >
+              <AiFillLinkedin
+                size={24}
+                className="hover:text-primary transition ease-in-out duration-200"
+              />
+            </a>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
 }
 
 // Exports
-export default Hero
+export default Hero;
