e29ba6fe7c21f819e32c91b13fd9d4f6299aae92
Duck1s <32653@ma-web.nl>
2022-10-25 12:43:54 +0200

    feat(meta): change meta data en skill indentation

--- categories/Skills.tsx
+++ categories/Skills.tsx
@@ -1,32 +1,47 @@
 // Imports
-import skills from "../public/json/skills.json"
+import skills from "../public/json/skills.json";
 
 // Functions
 function Skills() {
-	const skillList = skills.map((skill) => {
-		return (
-			<>
-				<li key={skill.id} className='bg-white rounded p-8 items-center flex flex-col justify-center text-center'>
-					<img src={skill.img} alt='Skill image icon' className='h-12 mb-4 object-fill' />
-					<p>{skill.name}</p>
-				</li>
-			</>
-		)
-	})
+  const skillList = skills.map((skill) => {
+    return (
+      <>
+        <li
+          key={skill.id}
+          className="bg-white rounded p-8 items-center flex flex-col justify-center text-center"
+        >
+          <img
+            src={skill.img}
+            alt="Skill image icon"
+            className="h-12 mb-4 object-fill"
+          />
+          <p>{skill.name}</p>
+        </li>
+      </>
+    );
+  });
 
-	return (
-		<>
-			<section className='page-padding-x page-padding-y bg-gray' id='skills'>
-				<h2 className='font-bold text-2xl mb-4'>Mijn skills</h2>
-				<p className='pb-10'>Dit is een lijst van technologiën waar ik mee kan werken.</p>
-				<ul className='grid grid-cols-2 md:grid-cols-4 gap-6 mb-8'>{skillList}</ul>
-				<a href='https://github.com/joeribreedveld' target='_blank' className='text-primary underline hover:text-secondary'>
-					Check mijn GitHub
-				</a>
-			</section>
-		</>
-	)
+  return (
+    <>
+      <section className="page-padding-x page-padding-y bg-gray" id="skills">
+        <h2 className="font-bold text-2xl mb-4">Mijn skills</h2>
+        <p className="pb-10">
+          Dit is een lijst van technologiën waar ik mee kan werken.
+        </p>
+        <ul className="grid grid-cols-2 md:grid-cols-4 gap-6 mb-8">
+          {skillList}
+        </ul>
+        <a
+          href="https://github.com/joeribreedveld"
+          target="_blank"
+          className="text-primary underline hover:text-secondary"
+        >
+          Check mijn GitHub
+        </a>
+      </section>
+    </>
+  );
 }
 
 // Exports
-export default Skills
+export default Skills;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,46 +1,65 @@
 // Imports
-import Head from "next/head"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 // Props
 type LayoutProps = {
-	children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 // Functions
 function Layout({ children }: LayoutProps) {
-	return (
-		<>
-			<Head>
-				<meta charSet='UTF-8' key='charset' />
-				<meta name='viewport' content='width=device-width,initial-scale=1' key='viewport' />
-				<meta name='description' content='Joeri Breedveld Full Stack Developer Portfolio Website' />
-				<link rel='icon' href='/favicon.ico' />
-				<title>Joeri Breedveld | Full Stack Developer</title>
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" key="charset" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+          key="viewport"
+        />
+        <meta
+          name="description"
+          content="Joeri Breedveld Full Stack Developer Portfolio Website"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Joeri Breedveld | Full Stack Developer</title>
 
-				<meta property='og:url' content='https://joeribreedveld.com/' />
-				<meta property='og:type' content='website' />
-				<meta property='og:title' content='Joeri Breedveld | Full Stack Developer' />
-				<meta property='og:description' content="Hello, I'm a full stack JavaScript web developer based in Amsterdam. Take a look at my experience and projects!" />
-				<meta property='og:image' content='https://cdn.ttgtmedia.com/rms/onlineimages/code_g1019737194.jpg' />
-				<meta name='twitter:card' content='summary_large_image' />
-				<meta property='twitter:domain' content='joeribreedveld.com' />
-				<meta property='twitter:url' content='https://joeribreedveld.com/' />
-				<meta name='twitter:title' content='Joeri Breedveld | Full Stack Developer' />
-				<meta name='twitter:description' content="Hello, I'm a full stack JavaScript web developer based in Amsterdam. Take a look at my experience and projects!" />
-				<meta name='twitter:image' content='https://cdn.ttgtmedia.com/rms/onlineimages/code_g1019737194.jpg' />
-			</Head>
-			<header>
-				<Navbar />
-			</header>
-			<main className='min-h-screen'>{children}</main>
-			<footer className='bg-black py-3 page-padding-x text-white flex justify-center items-center'>
-				<Footer />
-			</footer>
-		</>
-	)
+        <meta property="og:url" content="https://joeribreedveld.com/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Joeri Breedveld | Full Stack Developer"
+        />
+        <meta
+          property="og:description"
+          content="Hallo, ik ben een full stack developer en ik woon in Amsterdam. Ik vind het leuk om nieuwe dingen te leren en sta altijd open voor uitdagingen."
+        />
+        <meta property="og:image" content="../public/img/metaimg.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="joeribreedveld.com" />
+        <meta property="twitter:url" content="https://joeribreedveld.com/" />
+        <meta
+          name="twitter:title"
+          content="Joeri Breedveld | Full Stack Developer"
+        />
+        <meta
+          name="twitter:description"
+          content="Hallo, ik ben een full stack developer en ik woon in Amsterdam. Ik vind het leuk om nieuwe dingen te leren en sta altijd open voor uitdagingen."
+        />
+        <meta name="twitter:image" content="../public/img/metaimg.jpg" />
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="min-h-screen">{children}</main>
+      <footer className="bg-black py-3 page-padding-x text-white flex justify-center items-center">
+        <Footer />
+      </footer>
+    </>
+  );
 }
 
 // Exports
-export default Layout
+export default Layout;
